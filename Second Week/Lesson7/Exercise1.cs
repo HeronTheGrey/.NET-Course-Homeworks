dc8b268327e6d005b315e5e3e36e533f48b50f47
HeronTheGrey <62250797+HeronTheGrey@users.noreply.github.com>
2020-08-08 17:19:58 +0200

    Create Exercise1.cs

---
+++ Second Week/Lesson7/Exercise1.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Lesson7
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            if(a==b)
+            {
+                Console.Write($"{a} i {b} są równe.");
+            }
+            else
+            {
+                Console.Write($"{a} i {b} nie są równe.");
+            }
+        }
+    }
+}
