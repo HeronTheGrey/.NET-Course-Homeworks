9fd0e2c48830572a1cd4750c9f73d277424935d4
HeronTheGrey <62250797+HeronTheGrey@users.noreply.github.com>
2020-08-08 16:16:02 +0200

    Create Exercise4.cs

---
+++ Second Week/Lesson 4/Exercise4.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Lesson4
+{
+    class Exercise4
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 10;
+            string someWords = "Szkoła Dotneta";
+            double doubleNumber = 12.5;
+            
+        }
+    }
+}
