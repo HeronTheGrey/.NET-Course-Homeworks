1daa568fa47c3226c339364e2665776b7cb20d5a
HeronTheGrey <62250797+HeronTheGrey@users.noreply.github.com>
2020-08-08 15:53:24 +0200

    Create Exercise1.cs

---
+++ Second Week/Lesson 4/Exercise1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TypyDanych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            string secondName;
+            int age;
+            char sex;
+            string pesel;
+            string staffID;
+            
+        }
+    }
+}
