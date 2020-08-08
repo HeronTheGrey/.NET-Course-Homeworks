4d7a11557845957b9a2015de81ca5bcdb0bc053c
HeronTheGrey <62250797+HeronTheGrey@users.noreply.github.com>
2020-08-08 15:59:37 +0200

    Create Exercise2.cs

---
+++ Second Week/Lesson 4/Exercise2.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Lesson4
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            
+            char firstLetter = 'x';
+            char secondLetter = 'y';
+            char thirdLetter = 'z';
+
+            Console.Write($"{thirdLetter}\n{secondLetter}\n{firstLetter}");
+
+        }
+    }
+}
